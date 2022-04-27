387cc926f656ec6bd40ddee34732619ec8b5d4bc
YottaYocta <chubbean.rq@gmail.com>
2022-04-27 15:12:06 -0500

    initial commit

---
+++ src/context.ts
@@ -0,0 +1,36 @@
+import { Vec2, Color } from './utils';
+
+export class Context {
+  #parent: HTMLElement;
+  #canvasElement: HTMLCanvasElement;
+  #ctx: CanvasRenderingContext2D;
+
+  constructor(parent: HTMLElement) {
+      this.#parent = parent;
+      this.#canvasElement = document.createElement('canvas'); 
+      let ctx = this.#canvasElement.getContext('2d');
+      if (ctx === null)
+        throw 'cannot create context';
+      this.#ctx = ctx;
+      parent.appendChild(this.#canvasElement);
+  }
+
+  resize(width: number, height: number) {
+    this.#canvasElement.width = width;
+    this.#canvasElement.height = height;
+  }
+
+  set(location: Vec2, color: Color) {
+    this.#ctx.fillStyle = `rgb(${color.x * 255}, ${color.y * 255}, ${color.z * 255})`;
+    this.#ctx.fillRect(location.x, location.y, 1, 1);
+  }
+
+  setFlip(location: Vec2, color: Color) {
+    this.#ctx.fillStyle = `rgb(${color.x * 255}, ${color.y * 255}, ${color.z * 255})`;
+    this.#ctx.fillRect(location.x, this.#canvasElement.height - 1 - location.y, 1, 1);
+  }
+
+  destruct() {
+    this.#parent.removeChild(this.#canvasElement);
+  }
+}
---
+++ src/utils.ts
@@ -0,0 +1,76 @@
+export class Vec2 {
+  x: number;
+  y: number;
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export type Color = Vec3;
+export class Vec3 {
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(x = 0, y = 0, z = 0) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  add(a: Vec3 | number) {
+    if (typeof a === 'number')
+      return new Vec3(this.x + a, this.y + a, this.z + a);
+    else
+      return new Vec3(this.x + a.x, this.y + a.y, this.z + a.z);
+  }
+
+  sub(a: Vec3 | number) {
+    if (typeof a === 'number')
+      return new Vec3(this.x - a, this.y - a, this.z - a);
+    else
+      return new Vec3(this.x - a.x, this.y - a.y, this.z - a.z);
+
+  }
+
+  mul(a: Vec3 | number) {
+    if (typeof a === 'number')
+      return new Vec3(this.x * a, this.y * a, this.z * a);
+    else
+      return new Vec3(this.x * a.x, this.y * a.y, this.z * a.z);
+  }
+
+  div(a: Vec3 | number) {
+    if (typeof a === 'number')
+      return new Vec3(this.x / a, this.y / a, this.z / a);
+    else
+      return new Vec3(this.x / a.x, this.y / a.y, this.z / a.z);
+  }
+
+  dot(a: Vec3) {
+    return this.x * a.x + this.y * a.y + this.z * a.z;
+  }
+
+  get mag() {
+    return Math.sqrt(this.dot(this));
+  }
+
+  norm() {
+    let mag = this.mag;
+    return new Vec3(this.x / mag, this.y / mag, this.z / mag);
+  }
+
+  clone() {
+    return new Vec3(this.x, this.y, this.z);
+  }
+};
+
+export class Ray {
+  origin: Vec3;
+  direction: Vec3;
+  constructor(origin: Vec3, direction: Vec3) {
+    this.origin = origin;
+    this.direction = direction;
+  }
+}
